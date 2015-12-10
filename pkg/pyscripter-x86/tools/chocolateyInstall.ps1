b0dbddea4721744e78c885b515e9ee83980d77f5
Paul LeBlanc <PLeBlanc@esri.com>
2015-12-09 23:45:10 -0800

    install

---
+++ pkg/pyscripter-x86/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'pyscripter-x86'
+  unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  fileType      = 'exe'
+  url           = 'http://sourceforge.net/projects/pyscripter/files/PyScripter-v2.6.0-Setup.exe/download'
+
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'PyScripter 2.6.0'
+  checksum      = '46934B00AE4AB7D496110CC881335B085C166B4E'
+  checksumType  = 'sha1'
+}
+
+Install-ChocolateyPackage @packageArgs
